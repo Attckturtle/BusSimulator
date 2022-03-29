01999253a126ca2306f0c4355828348757658d2c
Attckturtle <89983007+Attckturtle@users.noreply.github.com>
2022-03-29 16:30:11 +0800

    Add files via upload

--- busRegistrationApplication/busRegistrationApplication/busRegistrationApplication.cpp
+++ busRegistrationApplication/busRegistrationApplication/busRegistrationApplication.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 
 bool firstRide = true;
-std::string lastLocation = "sentosa";
-int chosenOption;
-
-void showOptions();
-void processChosenOption();
-void goingToSentosa();
-void goingToBukit();
-void goingToTiong();
-void ticketPriceCalculator(int a);
 
 class BusMaker {
 public:
@@ -21,25 +12,31 @@ public:
 	bool inService = true;
 };
 
+class walletMaker {
+public:
+	int depositedAmount = 500;
+	std::string lastLocation = "Sentosa";
+};
+
 BusMaker Bus1;
 BusMaker Bus2;
 BusMaker Bus3;
 
-int main() {
-	showOptions();
-	return 0;
-}
+walletMaker accountWallet;
+
+void showOptions();
+void goingToSentosa();
+void goingToBukit();
+void goingToTiong();
+bool ticketPriceCalculator(int a);
 
 void showOptions() {
+	int chosenOption;
 	std::cout << "Where would you like to go?\n";
 	std::cout << "1. Sentosa\n";
 	std::cout << "2. Bukit Timah\n";
 	std::cout << "3. Tiong Bahru\n";
 	std::cin >> chosenOption;
-	processChosenOption();
-}
-
-void processChosenOption() {
 	switch (chosenOption) {
 	case 1:
 		goingToSentosa();
@@ -53,67 +50,112 @@ void processChosenOption() {
 }
 
 void goingToSentosa() {
-	int a;
-	a = rand() % 1000 + 1;
-	if (a == 1) {
-		Bus1.inService = false;
-	}
-	
-	int b;
-	b = rand() % 40 + 1;
-	Bus1.freeSeats -= b;
-
-	if (Bus1.freeSeats != 0 && Bus1.inService) {
-		lastLocation = "Sentosa\n";
-		std::cout << "You rode the bus\n";
-		showOptions();
+	bool samePlace = ticketPriceCalculator(1);
+	if (samePlace) {
+		int a;
+		a = rand() % 1000 + 1;
+		if (a == 1) {
+			Bus1.inService = false;
+		}
+		int b;
+		if (Bus1.freeSeats != 0 && Bus1.inService) {
+			accountWallet.lastLocation = "Sentosa\n";
+			std::cout << "You rode the bus\n";
+			showOptions();
+		}
+		else {
+			std::cout << "The bus is either full or broken\n";
+			showOptions();
+		}
+		b = rand() % 40 + 1;
+		Bus1.freeSeats -= b;
 	}
 	else {
-		std::cout << "The bus is either full or broken\n";
 		showOptions();
 	}
 }
 
 void goingToBukit() {
-	int a;
-	a = rand() % 1000 + 1;
-	if (a == 1) {
-		Bus2.inService = false;
+	bool samePlace = ticketPriceCalculator(2);
+	if (samePlace) {
+		int a;
+		a = rand() % 1000 + 1;
+		if (a == 1) {
+			Bus2.inService = false;
+		}
+		if (Bus2.inService) {
+			accountWallet.lastLocation = "Bukit Timah";
+		}
+
+		if (Bus2.freeSeats != 0 && Bus2.inService) {
+			accountWallet.lastLocation = "Bukit Timah";
+			std::cout << "You rode the bus\n";
+			showOptions();
+		}
+		else {
+			std::cout << "The bus is either full or broken\n";
+			showOptions();
+		}
 	}
-	if (Bus2.inService) {
-		lastLocation = "Bukit Timah\n";
+	else {
+		showOptions();
 	}
 }
 
 void goingToTiong() {
-	int a;
-	a = rand() % 1000 + 1;
-	if (a == 1) {
-		Bus3.inService = false;
+	bool samePlace = ticketPriceCalculator(3);
+	if (samePlace) {
+		int a;
+		a = rand() % 1000 + 1;
+		if (a == 1) {
+			Bus3.inService = false;
+		}
+		if (Bus3.inService) {
+			accountWallet.lastLocation = "Tiong Bahru\n";
+		}
+
+		if (Bus3.freeSeats != 0 && Bus3.inService) {
+			accountWallet.lastLocation = "Tiong Bahru";
+			std::cout << "You rode the bus\n";
+			showOptions();
+		}
+		else {
+			std::cout << "The bus is either full or broken\n";
+			showOptions();
+		}
 	}
-	if (Bus3.inService) {
-		lastLocation = "Tiong Bahru\n";
+	else {
+		showOptions();
 	}
 }
 
-void ticketPriceCalculator(int a) {
+bool ticketPriceCalculator(int a) {
 	std::string placeLocation;
 	
 	switch (a) {
 	case 1:
-		placeLocation = "Sentosa\n";
+		placeLocation = "Sentosa";
 		break;
 	case 2:
-		placeLocation = "Bukit Timah\n";
+		placeLocation = "Bukit Timah";
 		break;
 	case 3: 
-		placeLocation = "Tiong Bahru\n";
+		placeLocation = "Tiong Bahru";
 	}
 
-	if (lastLocation == placeLocation){
+	if (accountWallet.lastLocation == placeLocation){
 		std::cout << "You have to actually go somewhere\n";
+		return false;
 	}
 	else {
 		std::cout << "Tickets are $5\n";
+		accountWallet.depositedAmount -= 5;
+		std::cout << accountWallet.depositedAmount << " dollars left in your wallet.";
+		return true;
 	}
+}
+
+int main() {
+	showOptions();
+	return 0;
 }
